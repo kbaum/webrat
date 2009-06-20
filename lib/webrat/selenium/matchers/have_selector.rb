aff8984775baf196d6a06eca8ebd66acb54dd757
Pelle Braendgaard <pelleb@gmail.com>
2009-06-19 20:07:44 -0400

    Improved have_selector and have_tag in selenium to work fairly similar to the regular versions.

--- lib/webrat/selenium/matchers/have_selector.rb
+++ lib/webrat/selenium/matchers/have_selector.rb
@@ -2,16 +2,34 @@ module Webrat
   module Selenium
     module Matchers
       class HaveSelector
-        def initialize(expected)
+        def initialize(expected, options = {}, &block)
           @expected = expected
+          @options  = options
+          @block    = block
+          @content  = @options.delete(:content)||@options.delete(:text)
         end
 
         def matches?(response)
           response.session.wait_for do
-            response.selenium.is_element_present("css=#{@expected}")
+#            if @content
+#              @element=response.selenium.get_text("css=#{@expected}")
+#              case @content
+#              when String
+#                @element.include?(@content)
+#              when Regexp
+#                @element.match(@content)
+#              end
+#              
+#            else
+              response.selenium.is_element_present(selector)
+#            end
           end
           rescue Webrat::TimeoutError
             false
+        end
+        
+        def selector
+          "css=#{@expected}" + (@content && ":contains('#{@content}')")
         end
 
         # ==== Returns
@@ -27,21 +45,28 @@ module Webrat
         end
       end
 
-      def have_selector(content)
-        HaveSelector.new(content)
+      # Matches HTML content against a CSS 3 selector.
+      #
+      # ==== Parameters
+      # expected<String>:: The CSS selector to look for.
+      #
+      # ==== Returns
+      # HaveSelector:: A new have selector matcher.
+      def have_selector(name, attributes = {}, &block)
+        HaveSelector.new(name, attributes, &block)
       end
 
       # Asserts that the body of the response contains
       # the supplied selector
-      def assert_have_selector(expected)
-        hs = HaveSelector.new(expected)
+      def assert_have_selector(expected, attributes = {})
+        hs = HaveSelector.new(expected, attributes)
         assert hs.matches?(response), hs.failure_message
       end
 
       # Asserts that the body of the response
       # does not contain the supplied string or regepx
-      def assert_have_no_selector(expected)
-        hs = HaveSelector.new(expected)
+      def assert_have_no_selector(expected, attributes = {})
+        hs = HaveSelector.new(expected, attributes)
         assert !hs.matches?(response), hs.negative_failure_message
       end
     end
